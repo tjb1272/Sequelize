0bf14f8bd7e586f4d1a4c3a2eab72e485627c99c
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2018-02-01 17:27:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const express = require('express')
-const sqlite3 = require('sqlite3').verbose()
-const bodyParser = require('body-parser')
-const app = express()
-const handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+const app = express();
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
 const Sequelize = require('sequelize');
 const router = express.Router();
 
 
-app.engine('handlebars', handlebars.engine)
-app.set('view engine', 'handlebars')
-app.use(bodyParser.json())
-app.set('port', process.env.PORT || 3000)
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 3000);
+
 
 //Header
 var http = require('http');
 var options = {method: 'HEAD', port: 3000, path: '/'};
 var req = http.request(options, function(res) {
-    console.log(JSON.stringify(res.headers));
-  }
-);
+  console.log(JSON.stringify(res.headers));
+});
 req.end();
+
 
 //Connection
 const sequelize = new Sequelize('Music', 'tjb1272', null, {
@@ -29,49 +30,54 @@ const sequelize = new Sequelize('Music', 'tjb1272', null, {
   storage: './Chinook_Sqlite_AutoIncrementPKs.sqlite'
 });
 
+
 //Models
-const Artist = sequelize.define('Artist', {
-  ArtistId: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
+const Artist = sequelize.define(
+  "Artist",
+  {
+    ArtistId: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    Name: Sequelize.STRING
   },
-  Name: Sequelize.STRING
-},
-{
-freezeTableName: true,
-timestamps: false
-})
+  {
+    freezeTableName: true,
+    timestamps: false
+  }
+);
 
-const Album = sequelize.define('Album', {
-  ArtistId: {
-    type: Sequelize.INTEGER,
-    // autoIncrement: true,
-    primaryKey: true
+const Album = sequelize.define(
+  "Album",
+  {
+    AlbumId: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    Title: Sequelize.STRING
   },
-  Title: Sequelize.STRING
-},
-{
-freezeTableName: true,
-timestamps: false
-})
+  {
+    freezeTableName: true,
+    timestamps: false
+  }
+);
+Artist.hasMany(Album, { foreignKey: "ArtistId" });
+Album.belongsTo(Artist, { foreignKey: "ArtistId" });
 
-//Associations/Relationships
-Artist.hasMany(Album, {foreignKey: 'ArtistId'})
-Album.belongsTo(Artist, {foreignKey: 'ArtistId'})
-
-
-app.get('/album', (req, res) => {
- Album.findAll().then(albums => {
-   Artist.findAll().then(artists => {  
-     limit: 100;    
-     where: { ArtistId: req.params['id'] }
-     res.render('home', { artists: artists, albums: albums });
-     console.log(artists);
-     console.log(albums);
-   });
- });
+app.get("/album", (request, response) => {
+  Album.findAll({
+    include: [
+      {
+        model: Artist
+      }
+    ]
+  }).then(albums => {
+    response.render('home', { albums: albums });
+  });
 });
+
 
 app.use((req, res) => {
   res.status(400);
@@ -81,4 +87,4 @@ app.use((req, res) => {
 
 app.listen(3000, () => {
   console.log('server running')
-})
+});
